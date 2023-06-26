548cc2d56b93626c560ebc43bf240721f6b121c3
Chenxi Li <ayanamists@gmail.com>
2023-06-26 17:41:04 +0800

    remove useless debug message

--- packages/oh-my-old-tweet/src/App.tsx
+++ packages/oh-my-old-tweet/src/App.tsx
@@ -31,7 +31,6 @@ function App() {
           dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           placeholder={inputValue}
           onChange={(evt) => { 
-            console.log(evt);
             setInputValue(evt.target.value ?? ""); }} />
       </div>
       <div className='mx-auto text-center my-4'>

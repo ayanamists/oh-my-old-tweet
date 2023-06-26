5bf4ba6b47c759f0595949c94c6e3aef7028a198
Chenxi Li <ayanamists@gmail.com>
2023-06-26 16:37:53 +0800

    fix timeline user not set

--- packages/oh-my-old-tweet/src/App.tsx
+++ packages/oh-my-old-tweet/src/App.tsx
@@ -30,7 +30,9 @@ function App() {
           min-w-0 text-sm border-gray-300 p-2.5
           dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           placeholder={inputValue}
-          onChange={(evt) => { setInputValue(evt.target.value ?? ""); }} />
+          onChange={(evt) => { 
+            console.log(evt);
+            setInputValue(evt.target.value ?? ""); }} />
       </div>
       <div className='mx-auto text-center my-4'>
         <button className="bg-transparent hover:bg-tw-blue text-blue-700 
--- packages/oh-my-old-tweet/src/Timeline.tsx
+++ packages/oh-my-old-tweet/src/Timeline.tsx
@@ -2,19 +2,19 @@ import { useEffect, useState } from "react";
 import { filterUniqueCdxItems, getCdxItemId, getCdxList } from "./Data";
 import { LoadableTCard } from "./LoadableTCard";
 
-export function Timeline({ user }: { user : String }) {
+export function Timeline({ user }: { user : string }) {
     let [lst, setLst] = useState<JSX.Element[]>([])
 
     useEffect(() => {
         const f = async () => {
-            const cdxData = await getCdxList("_iori_n");
+            const cdxData = await getCdxList(user);
             const l = filterUniqueCdxItems(cdxData)
                 .map((i) => <LoadableTCard cdxItem={i} key={getCdxItemId(i)} />);
             setLst(l);
         }
 
         f()
-    }, []);
+    }, [user]);
 
     return (<ul className='App'>
         {lst}

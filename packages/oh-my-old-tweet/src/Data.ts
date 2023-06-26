5384bde5d8b0a325f6d5212f47ce09cbb154511b
Chenxi Li <ayanamists@gmail.com>
2023-06-26 20:25:03 +0800

    remove mock data

--- packages/oh-my-old-tweet/src/Data.ts
+++ packages/oh-my-old-tweet/src/Data.ts
@@ -1,42 +1,6 @@
 import Post from "./Post";
 import getUrl from "./corsUrl";
 
-// only for debugging
-// type Data = {
-//   name: string;
-//   title: string;
-//   user: string;
-//   image_paths: string[];
-// }
-
-// export function getPosts(limit: number): Promise<Post[]> {
-// const url = "http://localhost:5000/status/all"
-// const req = `${url}/${limit}`
-// return getData(req)
-// }
-// 
-// export function getPostsByUser(id: string) {
-// const url = "http://localhost:5000/status/user"
-// const req = `${url}/${id}`
-// return getData(req)
-// }
-
-// function getData(req: string) {
-// return fetch(req)
-// .then(res => res.json())
-// .then(j => {
-// let data = j as [Data]
-// return data.map(i => new Post(new User(i.name, undefined), i.title,
-// i.image_paths.map(p => {
-// console.log(p)
-// let img = new Image();
-// let url = `http://localhost:5000${p}`;
-// img.src = url;
-// return ({ path: url, width: img.width, height: img.height });
-// })))
-// })
-// }
-
 export function filterUniqueCdxItems(cdxItems: string[][]) {
   const idSet = new Set<String>();
   const res: string[][] = [];

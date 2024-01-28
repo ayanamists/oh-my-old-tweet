27202c792aec62e35326ef3bf15de53d4f7d3af5
Chenxi Li <ayanamists@gmail.com>
2024-01-28 19:49:51 +0800

    fix CopyShareLink on dark mode

--- packages/react-tweet-card/src/components/CopyShareLink/CopyShareLink.tsx
+++ packages/react-tweet-card/src/components/CopyShareLink/CopyShareLink.tsx
@@ -4,6 +4,26 @@ type Props = {
   shareLink: string
 }
 
+function MarkSymbol() {
+  return (
+    <svg width="40" height="40" viewBox="0 0 40 40">
+      <circle cx="20" cy="20" r="18" fill="none" stroke="silver" strokeWidth="1" />
+      <polyline points="15 26 23 34 37 20" fill="none" stroke="green" strokeWidth="2" />
+    </svg>
+  );
+}
+
+function CopySymbol() {
+  return (
+    <svg width="40" height="40" viewBox="0 0 40 40">
+      <circle cx="20" cy="20" r="18" stroke="silver" strokeWidth="1" fill="none" />
+      <text x="50%" y="50%" textAnchor="middle" dy=".3em" fontSize="20" color="green">
+        🔗
+      </text>
+    </svg>
+  );
+}
+
 export default function CopyShareLink({ shareLink }: Props) {
   const [copied, setCopied] = useState(false);
 
@@ -14,12 +34,7 @@ export default function CopyShareLink({ shareLink }: Props) {
 
   return (
     <button onClick={copyToClipboard} style={{ background: 'none', border: 'none' }} type="button">
-      <svg width="40" height="40" viewBox="0 0 40 40">
-        <circle cx="20" cy="20" r="18" stroke="silver" strokeWidth="1" fill="none" />
-        <text x="50%" y="50%" textAnchor="middle" dy=".3em" fontSize="20" color="black">
-          {copied ? '✔' : '🔗'}
-        </text>
-      </svg>
+      {copied ? <MarkSymbol /> : <CopySymbol />}
     </button>
   );
 }

137a635c31b4276999e3cacc5f5fd35f32f13add
Chenxi Li <ayanamists@gmail.com>
2025-05-09 16:52:30 +0800

    feat: add date range filtering with DateTime support

--- packages/oh-my-old-tweet/src/context/FilterContext.tsx
+++ packages/oh-my-old-tweet/src/context/FilterContext.tsx
@@ -1,30 +1,34 @@
-import { createContext, useCallback, useState } from "react";
+import { DateTime } from "luxon";
+import { createContext, useState } from "react";
 
 type ContentType = "reply" | "post"
 
+type DateOrAny = DateTime | "any"
+
 export interface TweetFilter {
-    contentBelongTo: ContentType[]
-    mustContainImage: boolean
-    dateInRange?: [Date, Date]
+  contentBelongTo: ContentType[]
+  mustContainImage: boolean
+  dateInRange: [DateOrAny, DateOrAny]
 }
 
 interface FilterContext {
-    tweetFilter: TweetFilter
-    setTweetFilter: (_: TweetFilter) => void
+  tweetFilter: TweetFilter
+  setTweetFilter: (_: TweetFilter) => void
 }
 
 const initFilter: TweetFilter = {
-    contentBelongTo: [ "reply", "post" ],
-    mustContainImage: false
+  contentBelongTo: ["reply", "post"],
+  mustContainImage: false,
+  dateInRange: ["any", "any"]
 };
 
 export const FilterContext = createContext<FilterContext>(
-    { tweetFilter: initFilter, setTweetFilter: () => {} });
+  { tweetFilter: initFilter, setTweetFilter: () => {} });
 
-export function FilterContextProvider({children}: {children: React.ReactNode}) {
-    const [ filter, setFilter ] = useState<TweetFilter>(initFilter);
+export function FilterContextProvider({ children }: { children: React.ReactNode }) {
+  const [filter, setFilter] = useState<TweetFilter>(initFilter);
 
-    return (<FilterContext.Provider value={{ tweetFilter: filter, setTweetFilter: setFilter }}>
-        {children}
-    </FilterContext.Provider>)
+  return (<FilterContext.Provider value={{ tweetFilter: filter, setTweetFilter: setFilter }}>
+    {children}
+  </FilterContext.Provider>)
 }
